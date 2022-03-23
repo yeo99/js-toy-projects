eb668a69556e09770313abf8d6fce0795770fdae
여승철 <timek70@dalcomsoft.com>
2022-03-23 15:17:42 +0900

    Todo list에 CREATE

--- 1-ToDoList/script.js
+++ 1-ToDoList/script.js
@@ -9,7 +9,10 @@
     return document.querySelector(target)
   }
 
-  const $todos = get('.todos')
+  const $todos = get('.todos');
+  // 할 일 입력했을때 서버로 데이터 쏴주도록 하기위해 form의 DOM 가져옴
+  const $form = get('.todo_form');
+  const $todoInput = get('.todo_input');
 
   const createTodoElement = (item) => {
     const { id, content } = item
@@ -71,11 +74,41 @@
       .catch((error) => console.error(error));
   }
 
+  // 새로고침됨. Ajax통신할건데 새로고침이 굳이 필요 없으므로 preventDefault() 추가
+  const addTodo = (e) => {
+    e.preventDefault();
+    
+    // 서버에 저장할 todo에는 content, completed, (id는 자동으로 들어감)
+    const todo = {
+      content: $todoInput.value,
+      completed: false,
+    }
+
+    // $form에서 submit 이벤트가 발생할 경우, 이벤트리스너로 인해 addTodo()호출, $todoInput에 입력된 value값이 콘솔에 찍힘
+    // console.log($todoInput.value);
+    fetch('http://localhost:3000/todos', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(todo),
+      // CREATE하고 나서 다시 투두리스트를 받아옴
+    }).then(getTodos)
+      .then(() => {
+        // CREATE시 Input 입력칸 비워주고
+        $todoInput.value = '';
+        // focus를 Input 입력칸으로 잡아줌
+        $todoInput.focus();
+      })
+  }
+
   const init = () => {
     // 파싱이 완료되었을 때 getTodos()함수 호출
     window.addEventListener('DOMContentLoaded', () => {
       getTodos();
     })
+    // submit 이벤트가 발생할 경우, addTodo()함수를 호출
+    $form.addEventListener('submit', addTodo);
   }
   init()
 })()
