447514632fab3b69af51a0a4119c3c211e469799
LAPTOP-여승철\yeo <timek70@onoffmix.com>
2022-09-01 16:41:57 +0900

    모달 클로즈 이벤트 추가

--- modal-popup/script.js
+++ modal-popup/script.js
@@ -8,6 +8,8 @@
   const $button = get('.modal_open_button');
   const $modal = get('.modal');
   const $body = get('body');
+  const $modalCancleButton = get('.modal_button.cancel');
+  const $modalConfirmButton = get('.modal_button.confirm');
 
   const toggleModal = () => {
     $modal.classList.toggle('show');
@@ -15,7 +17,22 @@
   }
 
   $button.addEventListener('click', () => {
-    toggleModal()
+    toggleModal();
   });
 
+  $modalCancleButton.addEventListener('click', () => {
+    toggleModal();
+  })
+
+  $modalConfirmButton.addEventListener('click', () => {
+    toggleModal();
+  })
+
+  // 모달 영역 밖을 클릭할 시 모달 창 닫음
+  window.addEventListener('click', (e) => {
+    console.log(e.target);
+    if (e.target === $modal) {
+      toggleModal();
+    }
+  })
 })()
